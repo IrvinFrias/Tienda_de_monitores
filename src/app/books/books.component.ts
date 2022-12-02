b8aa56d082af8c59d77abdf9231f03cbf04887ba
Irvin Fernando Frias Torres <irvin.frias.unam@outlook.com>
2022-12-01 18:18:06 -0600

    UI created

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,4 +1,10 @@
 import {Component, OnInit} from '@angular/core';
+interface Monitor{
+  alt: string
+  modelo: string
+  src: string
+  price: number
+}
 
 @Component({
   selector: 'app-books',
@@ -6,24 +12,41 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./books.component.css']
 })
 export class BooksComponent implements OnInit{
-  alt: string = 'Monitor'
-  modelo: string = 'Monitor Dell 24 - P2422H'
-  src: string = 'https://snpi.dell.com/snp/images/products/large/es-es~210-AZYX/210-AZYX.jpg'
+  monitors: Monitor[] = [
+    {
+      alt: 'monitor 24 inches',
+      modelo: 'Monitor Dell 24 - P2422H',
+      src: 'https://snpi.dell.com/snp/images/products/large/es-es~210-AZYX/210-AZYX.jpg',
+      price:1000
+
+    },
+    {
+      alt: 'monitor 24 inches',
+      modelo: ' monitor Dell UltraSharp 24 - U2422H',
+      src: 'https://snpi.dell.com/snp/images/products/large/es-es~210-AYUI/210-AYUI.JPG',
+      price:2000
+
+    },
+    {
+      alt: 'monitor 24 inches',
+      modelo: ' monitor Dell 24 - S2421HS',
+      src: 'https://snpi.dell.com/snp/images/products/large/es-es~210-AXKQ_V4/210-AXKQ_V4.jpg',
+      price:3000
+    },
+    {
+      alt: 'monitor 24 inches',
+      modelo: ' monitor Dell 24 con concentrador USB-C: P2423DE',
+      src: 'https://snpi.dell.com/snp/images/products/large/es-es~210-BDDW/210-BDDW.jpg',
+      price:4000
+    }
+  ]
+
+
   constructor() {
   }
-  alt2: string = 'Monitor';
-  modelo2: string = ' Monitor Dell UltraSharp 24 - U2422H';
-  src2: string = 'https://snpi.dell.com/snp/images/products/large/es-es~210-AYUI/210-AYUI.JPG'
 
-  isDisable: boolean = false;
+  isShowing: boolean = true;
 
-  handleClick(): void{
-    console.log('Hola mundo');
-  }
-  inputName: string = '';
-  handleInput(event: any): void{
-    this.inputName = event.target.value;
-  }
 
   ngOnInit(): void{
 
