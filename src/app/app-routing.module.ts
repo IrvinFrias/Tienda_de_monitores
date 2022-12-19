6e6340e9253b934ab472c48bdfb4fa2d7b96858c
Irvin Fernando Frias Torres <irvin.frias.unam@outlook.com>
2022-12-18 20:45:18 -0600

    nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {BooksComponent} from "./books/books.component";
 import {CardComponent} from "./card/card.component";
+import {LoginComponent} from "./auth/login/login.component";
+import {RegisterComponent} from "./auth/register/register.component";
 
 const routes: Routes = [
   {path: '', component: BooksComponent },
-  {path: 'card', component: CardComponent}
+  {path: 'card', component: CardComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent}
 ];
 
 @NgModule({
